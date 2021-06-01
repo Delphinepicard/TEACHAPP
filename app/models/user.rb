18434cb311759a2c180e685bf71f429fcb909725
Delphine Picard <delphinepicard.sk@gmail.com>
2021-06-01 14:29:49 +0200

    add validates for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ApplicationRecord
 
   has_one :attached_school_user, -> { where(school_users: { attachment: true }) }, class_name: 'SchoolUser'
   has_one :attached_school, through: :attached_school_user, source: :school
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :phone, presence: true, format: { with: /(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}/ }
 end
