c1bc807d4ec1fcf3e6783409a786e4342a9157bb
Delphine Picard <delphinepicard.sk@gmail.com>
2021-06-01 16:52:25 +0200

    add seed school

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_121244) do
+ActiveRecord::Schema.define(version: 2021_06_01_140354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,9 +77,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_121244) do
     t.string "name"
     t.string "address"
     t.string "specification"
-    t.bigint "director_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "director_id"
     t.string "phone"
     t.float "long"
     t.float "lat"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,42 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-SchoolTeacher.destroy_all
-Teacher.destroy_all
+SchoolUser.destroy_all
+User.destroy_all
 School.destroy_all
 Director.destroy_all
 
+# DB for schools in 42
+school_filepath = File.join(__dir__,'data/schools_42.csv')
 
-d1 = Director.create!(name: 'toto le directeur', phone: '06123456789', email: 'toto@gmai.com')
-d2 = Director.create!(name: 'titi le directeur', phone: '06123456789', email: 'titi@gmai.com')
+  sql = <<-SQL
+  COPY public.schools (name, address, phone, specification, lat, long)
+  FROM '#{school_filepath}'
+  DELIMITER ','
+  CSV HEADER QUOTE '"'
+  SQL
 
-
-school1 = School.create!(name: 'school1', director: d1)
-school2 = School.create!(name: 'school2', director: d2)
-school3 = School.create!(name: 'school3', director: d1)
-
-
-t1 = Teacher.create!(name: 'name1', mail: 't1@gmail.com', availability: true)
-t2 = Teacher.create!(name: 'name2', mail: 't2@gmail.com', availability: true)
-t3 = Teacher.create!(name: 'name3', mail: 't3@gmail.com', availability: true)
+  ActiveRecord::Base.connection.execute(sql)
 
 
-SchoolTeacher.create!(teacher: t1, school: school1, attachment: false)
-SchoolTeacher.create!(teacher: t1, school: school2, attachment: true)
-SchoolTeacher.create!(teacher: t1, school: school3, attachment: false)
-SchoolTeacher.create!(teacher: t2, school: school1, attachment: true)
-SchoolTeacher.create!(teacher: t2, school: school2, attachment: false)
-SchoolTeacher.create!(teacher: t2, school: school3, attachment: false)
+
+# d1 = Director.create!(name: 'toto le directeur', phone: '06123456789', email: 'toto@gmai.com')
+# d2 = Director.create!(name: 'titi le directeur', phone: '06123456789', email: 'titi@gmai.com')
+
+
+# school1 = School.create!(name: 'school1', director: d1)
+# school2 = School.create!(name: 'school2', director: d2)
+# school3 = School.create!(name: 'school3', director: d1)
+
+
+# t1 = Teacher.create!(name: 'name1', mail: 't1@gmail.com', availability: true)
+# t2 = Teacher.create!(name: 'name2', mail: 't2@gmail.com', availability: true)
+# t3 = Teacher.create!(name: 'name3', mail: 't3@gmail.com', availability: true)
+
+
+# SchoolTeacher.create!(teacher: t1, school: school1, attachment: false)
+# SchoolTeacher.create!(teacher: t1, school: school2, attachment: true)
+# SchoolTeacher.create!(teacher: t1, school: school3, attachment: false)
+# SchoolTeacher.create!(teacher: t2, school: school1, attachment: true)
+# SchoolTeacher.create!(teacher: t2, school: school2, attachment: false)
+# SchoolTeacher.create!(teacher: t2, school: school3, attachment: false)
