c224cceb948bc3cb33507e028daca138f4e52fe6
Patrice GERIN <patouseul@yahoo.fr>
2021-06-02 17:52:31 +0200

    Ajax reviews with date and user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,13 +2,12 @@ class ReviewsController < ApplicationController
 
   def create
     @classroom = Classroom.find(params[:classroom_id])
-
     @review = Review.new(review_params)
     @review.classroom = @classroom
     @review.user = current_user
     @assignment = Assignment.find(params[:review][:assignment])
     if @review.save!
-      redirect_to assignment_path(@assignment)
+      redirect_to assignment_path(@assignment, anchor: "review-#{@review.id}")
     else
       render 'assignments/show'
     end
