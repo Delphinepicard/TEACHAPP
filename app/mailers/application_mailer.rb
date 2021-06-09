dd69af163bb4955797f06ebaaed84f8a9d0a74c9
Bertrand Micheletti <bertrandmicheletti@gmail.com>
2021-06-09 16:40:37 +0200

    navbar mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'teachapp.lewagon@gmail.com'
   layout 'mailer'
+#  def render_files
+#    attachments.inline['teachapplogo.svg'] = File.read(Rails.root.join('/app/assets/images/teachapplogo.svg'))
+#  end
 end
