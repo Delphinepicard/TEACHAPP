f452d280c93d6a175523be22020536f248139f99
Mathieu Jolly <mathieujolly69@gmail.com>
2021-06-11 14:19:23 +0200

    js wip

--- app/javascript/packs/init_card_animation.js
+++ app/javascript/packs/init_card_animation.js
@@ -1,7 +1,26 @@
+const isElementInViewport = (el) => {
+
+
+    const rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+
 const card_init = () => {
 
-  const school_card = document.querySelector("#school_card");
+  Array.from(document.querySelectorAll('.school_card')).forEach((el) => {
 
+    window.addEventListener('scroll', (e) => {
+      if (isElementInViewport(el))
+        el.classList.add('reveal')
+    })
+  })
 
 }
 
