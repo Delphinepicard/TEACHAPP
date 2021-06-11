1dcb127ba1dc63b75cd56155470d07866badc697
Mathieu Jolly <mathieujolly69@gmail.com>
2021-06-11 12:30:01 +0200

    all

---
+++ app/javascript/packs/init_card_animation.js
@@ -0,0 +1,10 @@
+const card_init = () => {
+
+  const school_card = document.querySelector("#school_card");
+
+
+}
+
+export { card_init }
+
+// position Y card < position Y du scroll ( window.pageYOffset )
